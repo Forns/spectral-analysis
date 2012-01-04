7ce1c405739dee6c57fd97677591237d25d7bfe4
Forns <forneyandrew@gmail.com>
2012-01-04 00:46:11 -0800

    - Bootstrapping for the initial spectral analyzer; added node app definition, simple route, and placeholder data collector

---
+++ src/main/public/js/display/index-display.js
@@ -0,0 +1,14 @@
+/**
+ * index-display.js
+ * 
+ * Manages display elements on the index view.
+ */
+
+$(function () {
+  
+  // Set up the click functionality for the submit button
+  $("#submit-button").click(function () {
+    alert($("#data-entry").val());
+  });
+  
+});
